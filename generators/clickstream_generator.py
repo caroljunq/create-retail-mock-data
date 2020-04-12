9ec764770179d6d499a0a3c9fdde708cbdc9f4a2
caroljunq <junqueiracarolina@live.com>
2020-04-12 01:24:58 -0300

    clickstream okk

--- generators/clickstream_generator.py
+++ generators/clickstream_generator.py
@@ -21,9 +21,11 @@ outsize = config["clickstream"]["total"]
 click_start_date = config["clickstream"]["click_start_date"]
 click_end_date = config["clickstream"]["click_end_date"]
 
+index_clickstream_start = 1
+
 # config of media sources
 media_sources = config["media"]["sources"]
-media_prob = config["media"]["percentage"]
+media_prob = config["media"]["percentages"]
 
 # data processing config
 amounts_cpu = config["data_processing"]["amount_in_cpu"]
@@ -62,6 +64,49 @@ def generate_clickstream(amount,index_start):
         media = np.random.choice(media_sources, p=media_prob)  
         product_id = random.randint(index_start_product, index_start_product + n_products - 1) 
         results.append((customer_id,order_date,campaign_id,media,product_id))
+    
+    return results
+
+def collect_clickstream(results):
+    # collect intermediate results
+    global clickstream
+    # collect clickstream info and add to the clickstream array
+    clickstream = clickstream + list(results)
+
+    # print the process
+    print("{} processed".format(len(clickstream)))
+
+# setting the number of cores used by the process, aka how many processes will run in parallel 
+print("Initializing using {} cores".format(machine_cores))
+pool = mp.Pool(machine_cores)
+
+# numbers of generated items in each loop
+amounts = int(outsize/machine_cores) if auto_batch else amounts_cpu
+number_of_loops = int(outsize/amounts)
+residue = outsize - amounts * number_of_loops
+
+# first generating residue
+pool.apply_async(generate_clickstream, args=(residue, index_clickstream_start), callback=collect_clickstream)
+
+# generating clickstream in parallel 
+for i in range(number_of_loops):
+    pool.apply_async(generate_clickstream, args=(amounts, (i * amounts) + residue + index_clickstream_start), callback=collect_clickstream)
+
+# closing pool
+pool.close()
+pool.join()
+
+# creating a data frame with the final results
+df = pd.DataFrame(clickstream)
+
+# columns names aka header
+columns_names = ["customer_id","order_date","campaign_id","media_source","product_id"]
+
+print("Saving file...")
+
+# writing file
+f = df.to_csv(out_path + outfile,header=columns_names,sep=",",index=False)
+print("File was saved at path {}".format(out_path + outfile))
 
 
 
